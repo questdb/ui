b1369a64475890c4548ace7641650e71715903e1
argshook <argshook@gmail.com>
2022-03-04 12:57:46 +0200

    add comment about test setup

--- packages/browser-tests/cypress/integration/console/editor.spec.js
+++ packages/browser-tests/cypress/integration/console/editor.spec.js
@@ -1,10 +1,23 @@
 /// <reference types="cypress" />
 
-const queries = [
-  "select * from telemetry",
-  "select * from telemetry_config",
-  "--start\nselect * from telemetry_config\n--end",
-];
+/**
+ * These tests work only if `questdb/ui/assets/console-configuration.json`
+ * has the same `savedconsoleConfiguration.savedQueries` as the `consoleConfiguration` in this file.
+ *
+ * This is required, because currently questdb/ui does not have a programmatic way to inject this data in test
+ * environment
+ */
+
+const consoleConfiguration = {
+  savedQueries: [
+    { name: "query 1", value: "select * from telemetry" },
+    { name: "query 1", value: "select * from telemetry_config" },
+    {
+      name: "query 1",
+      value: "--start\nselect * from telemetry_config\n--end",
+    },
+  ],
+};
 
 describe("appendQuery", () => {
   beforeEach(() => {
@@ -34,7 +47,7 @@ describe("appendQuery", () => {
     cy.selectQuery(2);
     cy.typeQuery(`{ctrl}g2{enter}`); // go to line 2
     cy.selectQuery(1);
-    const expected = `${queries[1]}\n\n${queries[1]}\n\n--start\n${queries[1]}\n--end\n`;
+    const expected = `${consoleConfiguration.savedQueries[1].value}\n\n${consoleConfiguration.savedQueries[1].value}\n\n--start\n${consoleConfiguration.savedQueries[1].value}\n--end\n`;
     cy.getEditor().should("have.value", expected).snapshot();
   });
 
@@ -42,36 +55,36 @@ describe("appendQuery", () => {
     cy.typeQuery(`{enter}--b{upArrow}`);
     cy.selectQuery(0);
     cy.selectQuery(1);
-    const expected = `${queries[0]}\n\n${queries[1]}\n\n--b`;
+    const expected = `${consoleConfiguration.savedQueries[0].value}\n\n${consoleConfiguration.savedQueries[1].value}\n\n--b`;
     cy.getEditor().should("have.value", expected).snapshot();
   });
 
   it("should correctly append and add surrounding new lines when position is middle line which is empty", () => {
     cy.typeQuery(`--a{enter}{enter}--b{upArrow}`);
     cy.selectQuery(0);
-    const expected = `--a\n\n${queries[0]}\n\n--b`;
+    const expected = `--a\n\n${consoleConfiguration.savedQueries[0].value}\n\n--b`;
     cy.getEditor().should("have.value", expected).snapshot();
   });
 
   it("should correctly append and add surrounding new lines when position is last line which is empty", () => {
     cy.typeQuery(`--a{enter}--b`);
     cy.selectQuery(0);
-    const expected = `--a\n--b\n\n${queries[0]}\n`;
+    const expected = `--a\n--b\n\n${consoleConfiguration.savedQueries[0].value}\n`;
     cy.getEditor().should("have.value", expected).snapshot();
   });
 
   it("should correctly append and add surrounding new lines when there are two lines and position is last line which is empty", () => {
     cy.typeQuery(`--a{enter}`);
     cy.selectQuery(0);
-    const expected = `--a\n\n${queries[0]}\n`;
+    const expected = `--a\n\n${consoleConfiguration.savedQueries[0].value}\n`;
     cy.getEditor().should("have.value", expected).snapshot();
   });
 
-  it.only("should correctly append and add surrounding new lines when position is middle of non empty line and next line is empty", () => {
+  it("should correctly append and add surrounding new lines when position is middle of non empty line and next line is empty", () => {
     cy.typeQuery(`--a{enter}--b{enter}{enter}--c`);
     cy.typeQuery(`{ctrl}g2{enter}{rightArrow}`); // go to line 2
     cy.selectQuery(0);
-    const expected = `--a\n--b\n\n${queries[0]}\n\n--c`;
+    const expected = `--a\n--b\n\n${consoleConfiguration.savedQueries[0].value}\n\n--c`;
     cy.getEditor().should("have.value", expected).snapshot();
   });
 });
