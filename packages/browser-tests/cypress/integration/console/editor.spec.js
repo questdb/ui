59b52abd619b4f0bf1455d84026c43a28adb620b
argshook <argshook@gmail.com>
2022-03-04 18:50:40 +0200

    refactor tests

--- packages/browser-tests/cypress/integration/console/editor.spec.js
+++ packages/browser-tests/cypress/integration/console/editor.spec.js
@@ -2,14 +2,16 @@
 
 const consoleConfiguration = {
   savedQueries: [
-    { name: "query 1", value: "select * from telemetry" },
-    { name: "query 1", value: "select * from telemetry_config" },
+    { name: "query 1", value: "first query" },
+    { name: "query 1", value: "second query" },
     {
       name: "query 1",
-      value: "--start\nselect * from telemetry_config\n--end",
+      value: "multi\nline\nquery",
     },
   ],
 };
+
+const queries = consoleConfiguration.savedQueries.map((query) => query.value);
 
 describe("appendQuery", () => {
   beforeEach(() => {
@@ -26,28 +28,28 @@ describe("appendQuery", () => {
 
   it("should append and select query", () => {
     cy.selectQuery(0);
-    const expected = `select * from telemetry\n`;
+    const expected = `${queries[0]}\n`;
     cy.getEditor().should("have.value", expected).snapshot();
   });
 
   it("should append and select query", () => {
     cy.selectQuery(1);
-    const expected = `select * from telemetry_config\n`;
+    const expected = `${queries[1]}\n`;
     cy.getEditor().should("have.value", expected).snapshot();
   });
 
   it("should append and select multiline query", () => {
     cy.selectQuery(2);
-    const expected = `--start\nselect * from telemetry_config\n--end\n`;
+    const expected = `${queries[2]}\n`;
     cy.getEditor().should("have.value", expected).snapshot();
   });
 
-  it("should correctly append and select query after multiple inserts", () => {
+  it.only("should correctly append and select query after multiple inserts", () => {
     cy.selectQuery(1);
     cy.selectQuery(2);
     cy.typeQuery(`{ctrl}g2{enter}`); // go to line 2
     cy.selectQuery(1);
-    const expected = `${consoleConfiguration.savedQueries[1].value}\n\n${consoleConfiguration.savedQueries[1].value}\n\n--start\n${consoleConfiguration.savedQueries[1].value}\n--end\n`;
+    const expected = `${queries[1]}\n\n${queries[1]}\n\n${queries[2]}\n`;
     cy.getEditor().should("have.value", expected).snapshot();
   });
 
@@ -55,28 +57,28 @@ describe("appendQuery", () => {
     cy.typeQuery(`{enter}--b{upArrow}`);
     cy.selectQuery(0);
     cy.selectQuery(1);
-    const expected = `${consoleConfiguration.savedQueries[0].value}\n\n${consoleConfiguration.savedQueries[1].value}\n\n--b`;
+    const expected = `${queries[0]}\n\n${queries[1]}\n\n--b`;
     cy.getEditor().should("have.value", expected).snapshot();
   });
 
   it("should correctly append and add surrounding new lines when position is middle line which is empty", () => {
     cy.typeQuery(`--a{enter}{enter}--b{upArrow}`);
     cy.selectQuery(0);
-    const expected = `--a\n\n${consoleConfiguration.savedQueries[0].value}\n\n--b`;
+    const expected = `--a\n\n${queries[0]}\n\n--b`;
     cy.getEditor().should("have.value", expected).snapshot();
   });
 
   it("should correctly append and add surrounding new lines when position is last line which is empty", () => {
     cy.typeQuery(`--a{enter}--b`);
     cy.selectQuery(0);
-    const expected = `--a\n--b\n\n${consoleConfiguration.savedQueries[0].value}\n`;
+    const expected = `--a\n--b\n\n${queries[0]}\n`;
     cy.getEditor().should("have.value", expected).snapshot();
   });
 
   it("should correctly append and add surrounding new lines when there are two lines and position is last line which is empty", () => {
     cy.typeQuery(`--a{enter}`);
     cy.selectQuery(0);
-    const expected = `--a\n\n${consoleConfiguration.savedQueries[0].value}\n`;
+    const expected = `--a\n\n${queries[0]}\n`;
     cy.getEditor().should("have.value", expected).snapshot();
   });
 
@@ -84,7 +86,7 @@ describe("appendQuery", () => {
     cy.typeQuery(`--a{enter}--b{enter}{enter}--c`);
     cy.typeQuery(`{ctrl}g2{enter}{rightArrow}`); // go to line 2
     cy.selectQuery(0);
-    const expected = `--a\n--b\n\n${consoleConfiguration.savedQueries[0].value}\n\n--c`;
+    const expected = `--a\n--b\n\n${queries[0]}\n\n--c`;
     cy.getEditor().should("have.value", expected).snapshot();
   });
 });
