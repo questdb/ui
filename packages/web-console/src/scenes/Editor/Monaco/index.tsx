a4b76f76cc1c6f13845e23a427f7b3c37ba643cf
Maciej Bodek <maciej.bodek@gmail.com>
2022-09-26 19:42:18 +0200

    Do not duplicate existing queries when appending

--- packages/web-console/src/scenes/Editor/Monaco/index.tsx
+++ packages/web-console/src/scenes/Editor/Monaco/index.tsx
@@ -203,9 +203,18 @@ const MonacoEditor = () => {
 
     // Insert query, if one is found in the URL
     const params = new URLSearchParams(window.location.search)
-    const query = params.get("query")
+    // Support multi-line queries (URL encoded)
+    const query = params.get("query")?.replaceAll("%0A", "\n")
+    const model = editor.getModel()
     if (query) {
-      appendQuery(editor, query, { appendAt: "end" })
+      // Find if the query is already in the editor
+      const matches = model?.findMatches(query, true, false, true, null, true)
+      if (matches) {
+        editor.setSelection(matches[0].range)
+        // otherwise, append the query
+      } else {
+        appendQuery(editor, query, { appendAt: "end" })
+      }
     }
 
     const executeQuery = params.get("executeQuery")
