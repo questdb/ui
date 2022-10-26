78d503305b39ed70ad82c842a6a05af9107c941c
argshook <argshook@gmail.com>
2022-10-26 14:55:30 +0300

    for QueryPicker add new buffer if current non empty

--- packages/web-console/src/scenes/Editor/QueryPicker/index.tsx
+++ packages/web-console/src/scenes/Editor/QueryPicker/index.tsx
@@ -123,7 +123,7 @@ const QueryPicker = ({ hidePicker, queries, ref }: Props) => {
   const [queryList, setQueryList] = useState<
     ReturnType<typeof prepareQueriesList>
   >([])
-  const { addBuffer } = useEditor()
+  const { appendQuery, editorRef, addBuffer } = useEditor()
 
   useEffect(() => {
     setQueryList(prepareQueriesList(queries))
@@ -132,7 +132,12 @@ const QueryPicker = ({ hidePicker, queries, ref }: Props) => {
   const addQuery = useCallback(
     (query: Query) => {
       hidePicker()
-      addBuffer(makeBuffer({ label: query.name ?? "", value: query.value }))
+      // add new buffer when current one is not empty
+      if (editorRef.current && editorRef.current.getValue() !== "") {
+        addBuffer(makeBuffer({ label: query.name ?? "", value: query.value }))
+      } else {
+        appendQuery(query.value)
+      }
     },
     [hidePicker],
   )
