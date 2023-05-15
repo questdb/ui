edcac426f663337bdad7c92377b7f64d117e538d
Maciej Bodek <maciej.bodek@gmail.com>
2023-05-15 22:32:40 +0200

    cleanup: unused deps

--- packages/web-console/src/scenes/Import/ImportCSVFiles/table-schema/columns.tsx
+++ packages/web-console/src/scenes/Import/ImportCSVFiles/table-schema/columns.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react"
+import React, { useCallback, useEffect } from "react"
 import styled from "styled-components"
 import { useFieldArray, useFormContext } from "react-hook-form"
 import { Box } from "../../../../components/Box"
