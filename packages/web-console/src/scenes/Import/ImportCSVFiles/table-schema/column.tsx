bf885d7d51a106f634424f1b82b227f66d6cdd28
Maciej Bodek <maciej.bodek@gmail.com>
2023-05-16 00:42:17 +0200

    Allow editing column types on existing tables

--- packages/web-console/src/scenes/Import/ImportCSVFiles/table-schema/column.tsx
+++ packages/web-console/src/scenes/Import/ImportCSVFiles/table-schema/column.tsx
@@ -111,7 +111,6 @@ export const Column = ({
               </Form.Item>
               <Form.Item name={`schemaColumns.${index}.type`} label="Type">
                 <Form.Select
-                  disabled={isEditLocked}
                   defaultValue={column.type}
                   name={`schemaColumns.${index}.type`}
                   options={supportedColumnTypes}
@@ -122,7 +121,7 @@ export const Column = ({
               <IconWithTooltip
                 icon={
                   <Button
-                    disabled={type !== "TIMESTAMP" || isEditLocked}
+                    disabled={column.type !== "TIMESTAMP"}
                     skin={
                       timestamp !== "" &&
                       column.name !== "" &&
@@ -161,7 +160,6 @@ export const Column = ({
                 helperText="Required when using the TIMESTAMP type"
               >
                 <Form.Input
-                  disabled={isEditLocked}
                   name={`schemaColumns.${index}.pattern`}
                   placeholder={DEFAULT_TIMESTAMP_FORMAT}
                   defaultValue={
