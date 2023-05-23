40511f67756c00afd783702c2d9a1fbe432a2a63
Maciej Bodek <maciej.bodek@gmail.com>
2023-05-23 19:12:00 +0200

    adjust types

--- packages/web-console/src/components/TableSchemaDialog/types.ts
+++ packages/web-console/src/components/TableSchemaDialog/types.ts
@@ -3,8 +3,8 @@ export type Action = "add" | "import"
 export type SchemaColumn = {
   name: string
   type: string
-  pattern: string | undefined
-  precision: string | undefined
+  pattern?: string
+  precision?: string
 }
 
 export type SchemaFormValues = {
@@ -12,5 +12,5 @@ export type SchemaFormValues = {
   schemaColumns: SchemaColumn[]
   partitionBy: string
   timestamp: string
-  walEnabled: string | undefined
+  walEnabled?: string
 }
