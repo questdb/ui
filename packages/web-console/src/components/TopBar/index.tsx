bf7661322e251a61fa868cefb241f60316a068a8
Arijus Šukys <arijus@questdb.io>
2023-11-15 16:02:52 +0200

    Add 'Return to Cloud' Button (#232)
    * clean `Menu` component
    
    * remove unused declarations
    * remove unused imports
    * remove unused react state
    
    * add `BackButton` component
    
    * fix `PopperHover` modifiers spread
    
    when modifiers are spread first, they get overriden by default ones.
    
    * make space for `BackButton`
    
    * setup indexedDB to store `returnTo` and `returnToLabel`
    
    * read URL params, store to indexedDB and remove from URL
    
    * use `<a>` for linking
    
    * conditionally display return button in the topbar
    
    * fix IconWithTooltip modifier
    
    IconWithTooltip had `offset` modifier set, but it did not work, due to
    inability to override defaults.
    
    in commit adb6df41 the `modifiers` spread was
    fixed to allow overriding default modifiers (like `offset`, `arrow` and
    `eventListeners`).
    
    This made `offset` modifier set in `IconWithTooltip` to start working.
    But since it didn't work before, the change in commit adb6df41 unearthed
    a visual bug.
    
    thus, this commit removes the `offset` modifier set in `IconWithTooltip`
    to fix the visual bug.
    
    * fix typo in `align` prop
    
    * use `PrimaryToggleButton` in `BackButton`
    
    this way the style is consistent with other buttons
    
    ---------
    
    Co-authored-by: Maciej Bodek <1871646+insmac@users.noreply.github.com>

--- packages/web-console/src/components/TopBar/index.tsx
+++ packages/web-console/src/components/TopBar/index.tsx
@@ -3,20 +3,43 @@ import styled from "styled-components"
 import Menu from "../../scenes/Editor/Menu"
 import { Box } from "@questdb/react-components"
 import { Version } from "./version"
+import { BackButton } from "./back-button"
+import { useLiveQuery } from "dexie-react-hooks"
+import { db } from "../../store/db"
 
 const Root = styled(Box).attrs({
-  align: "cneter",
+  align: "center",
   justifyContent: "space-between",
 })`
   width: 100%;
   height: 4.5rem;
   background: ${({ theme }) => theme.color.backgroundDarker};
-  padding: 0 1rem;
 `
 
 export const TopBar = () => {
+  const editorSettings = useLiveQuery(
+    async () => ({
+      returnTo:
+        (await db.editor_settings.where("key").equals("returnTo").first())
+          ?.value ?? "",
+
+      returnToLabel:
+        (await db.editor_settings.where("key").equals("returnToLabel").first())
+          ?.value ?? "",
+    }),
+    [],
+  )
+
   return (
     <Root>
+      {editorSettings?.returnTo && (
+        <BackButton
+          label={editorSettings?.returnToLabel}
+          onClick={() => {
+            window.location.href = editorSettings.returnTo as string
+          }}
+        />
+      )}
       <Version />
       <Menu />
     </Root>
