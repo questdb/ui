a90a76f20e7ca2a06fa5135f3783549f4df66420
Maciej Bodek <maciej.bodek@gmail.com>
2023-11-29 17:25:55 +0100

    Update autocomplete tests

--- packages/browser-tests/cypress/integration/console/editor.spec.js
+++ packages/browser-tests/cypress/integration/console/editor.spec.js
@@ -194,20 +194,18 @@ describe("autocomplete", () => {
       .should("not.be.visible")
       .clearEditor();
 
-    cy.typeQuery('create table "my_secrets" ("secret" string)')
-      .runLine()
-      .clearEditor();
-
-    cy.typeQuery("select * from my_")
-      .getAutocomplete()
-      .should("contain", "my_secrets");
-
     cy.matchImageSnapshot();
-    cy.clearEditor().typeQuery('drop table "my_secrets"').runLine();
   });
 
-  it("should work when tables list is not empty", () => {
+  it.only("should work when tables list is not empty", () => {
     cy.typeQuery('create table "my_secrets" ("secret" string);')
+      .clickRun()
+      .clearEditor();
+
+    // We're creating another table with the same column name.
+    // The autocomplete should merge the column completions into one
+    // and respond with something like `secret (my_secrets, my_secrets2)`
+    cy.typeQuery('create table "my_secrets2" ("secret" string);')
       .clickRun()
       .clearEditor();
 
@@ -216,14 +214,34 @@ describe("autocomplete", () => {
       .clearEditor();
 
     cy.visit(baseUrl);
-    cy.typeQuery("\nselect * from ");
+    cy.typeQuery("\nselect ");
     cy.getAutocomplete()
+      // Tables
       .should("not.contain", "telemetry")
       .should("contain", "my_secrets")
       .should("contain", "my_publics")
+      // Columns
+      .should("contain", "secret")
+      .should("contain", "public")
+      // Tables list for the `secret` column
+      // list the tables containing `secret` column
+      .should("contain", "my_secrets, my_secrets2")
+      .clearEditor();
+
+    cy.typeQuery("select * from my_secrets where ");
+    cy.getAutocomplete()
+      .should("contain", "secret")
+      .should("not.contain", "public")
+      .clearEditor();
+
+    cy.typeQuery("select * from my_secrets join my_publics on ");
+    cy.getAutocomplete()
+      .should("contain", "my_publics.public")
+      .should("contain", "my_secrets.secret")
       .clearEditor();
 
     cy.typeQuery('drop table "my_secrets"').runLine().clearEditor();
+    cy.typeQuery('drop table "my_secrets2"').runLine().clearEditor();
     cy.typeQuery('drop table "my_publics"').runLine().clearEditor();
   });
 });
