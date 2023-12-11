02bb3102580a1806aefdb4be8da27dfd4415f214
Maciej Bodek <maciej.bodek@gmail.com>
2023-12-11 18:26:48 +0100

    Display result zero state at all times

--- packages/web-console/src/scenes/Console/zero-state.tsx
+++ packages/web-console/src/scenes/Console/zero-state.tsx
@@ -1,9 +1,9 @@
-import React, { useContext, useEffect, useState } from "react"
+import React from "react"
 import styled from "styled-components"
 import { Start } from "../../modules/ZeroState/start"
 import { PaneContent, PaneWrapper } from "../../components"
-import * as QuestDB from "../../utils/questdb"
-import { QuestContext } from "../../providers"
+import { selectors } from "../../store"
+import { useSelector } from "react-redux"
 
 const StyledPaneContent = styled(PaneContent)`
   align-items: center;
@@ -11,32 +11,11 @@ const StyledPaneContent = styled(PaneContent)`
 `
 
 export const ZeroState = () => {
-  const { quest } = useContext(QuestContext)
-  const [loading, setLoading] = useState(true)
-  const [tables, setTables] = useState<QuestDB.Table[]>([])
-
-  const fetchTables = async () => {
-    try {
-      const response = await quest.showTables()
-      if (response && response.type === QuestDB.Type.DQL) {
-        setTables(response.data)
-      }
-    } catch (error) {
-      return
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  useEffect(() => {
-    void fetchTables()
-  }, [])
+  const { readOnly } = useSelector(selectors.console.getConfig)
 
   return (
     <PaneWrapper>
-      <StyledPaneContent>
-        {!loading && tables.length <= 2 && <Start />}
-      </StyledPaneContent>
+      <StyledPaneContent>{!readOnly && <Start />}</StyledPaneContent>
     </PaneWrapper>
   )
 }
