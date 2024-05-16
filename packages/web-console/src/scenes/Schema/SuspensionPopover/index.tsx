15e2cb868d8a9f74182a9460ec9de94c5cd9be6d
Maciej Bodek <maciej.bodek@gmail.com>
2024-05-16 09:51:53 +0200

    Suspension popover: Add resume logic

--- packages/web-console/src/scenes/Schema/SuspensionPopover/index.tsx
+++ packages/web-console/src/scenes/Schema/SuspensionPopover/index.tsx
@@ -1,10 +1,14 @@
-import React, { useState } from "react"
+import React, { useState, useContext } from "react"
 import { PopperToggle, Text } from "../../../components"
 import { Form } from "../../../components/Form"
 import { Box, Button } from "@questdb/react-components"
 import styled from "styled-components"
 import { ArrowDownS, ExternalLink, Restart } from "@styled-icons/remix-line"
 import * as QuestDB from "../../../utils/questdb"
+import Joi from "joi"
+import { QuestContext } from "../../../providers"
+import { eventBus } from "../../../modules/EventBus"
+import { EventType } from "../../../modules/EventBus/types"
 
 const Root = styled.div`
   display: flex;
@@ -65,12 +69,30 @@ const TransactionInput = styled(Form.Input)`
   width: 10rem;
 `
 
+type FormValues = {
+  resume_transaction_id?: number
+}
+
 export const SuspensionPopover = ({
   walTableData,
 }: {
   walTableData: QuestDB.WalTable
 }) => {
   const [active, setActive] = useState(false)
+  const { quest } = useContext(QuestContext)
+
+  const handleSubmit = async (values: FormValues) => {
+    const response = await quest.query(
+      `ALTER TABLE ${walTableData.name} RESUME WAL${
+        values.resume_transaction_id
+          ? ` FROM TRANSACTION ${values.resume_transaction_id}`
+          : ""
+      }`,
+    )
+    if (response && response.type === QuestDB.Type.DDL) {
+      eventBus.publish(EventType.MSG_QUERY_SCHEMA)
+    }
+  }
 
   return (
     <PopperToggle
@@ -109,8 +131,15 @@ export const SuspensionPopover = ({
             </StyledTable>
           </ContentBlockBox>
           <ContentBlockBox gap="0.5rem">
-            <Text color="gray2">Start txn #:</Text>
-            <Form name="resume_transaction_form" onSubmit={() => {}}>
+            <Text color="gray2">Start at (optional):</Text>
+            <Form<FormValues>
+              name="resume_transaction_form"
+              onSubmit={handleSubmit}
+              defaultValues={{ resume_transaction_id: undefined }}
+              validationSchema={Joi.object({
+                resume_transaction_id: Joi.number().optional().allow(""),
+              })}
+            >
               <FormWrapper>
                 <Form.Item name="resume_transaction_id">
                   <TransactionInput
