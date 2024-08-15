afd1c8d0f4596d2c0c9f297a52415287d2162bc7
Maciej Bodek <maciej.bodek@gmail.com>
2024-08-15 10:54:04 +0200

    Adjust sample by based on time range

--- packages/web-console/src/scenes/Schema/Table/queries.ts
+++ packages/web-console/src/scenes/Schema/Table/queries.ts
@@ -1,7 +1,21 @@
-import { durationInMinutes } from "./../../../modules/Graph/types"
-import { MetricDuration } from "modules/Graph/types"
+import {
+  durationInMinutes,
+  MetricDuration,
+} from "./../../../modules/Graph/types"
 
 const minutesToDays = (durationInMinutes: number) => durationInMinutes / 60 / 24
+
+export const mappedSampleBy: Record<MetricDuration, string> = {
+  [MetricDuration.TEN_MINUTES]: "1m",
+  [MetricDuration.THIRTY_MINUTES]: "1m",
+  [MetricDuration.ONE_HOUR]: "1m",
+  [MetricDuration.THREE_HOURS]: "1m",
+  [MetricDuration.SIX_HOURS]: "1m",
+  [MetricDuration.TWELVE_HOURS]: "1m",
+  [MetricDuration.TWENTY_FOUR_HOURS]: "1m",
+  [MetricDuration.THREE_DAYS]: "15m",
+  [MetricDuration.SEVEN_DAYS]: "15m",
+}
 
 export const rowsApplied = (id: string, metricDuration: MetricDuration) => `
 select
@@ -17,7 +31,7 @@ and created > date_trunc('minute', dateadd('d', -${minutesToDays(
   durationInMinutes[metricDuration],
 )}, now()))
 and created < date_trunc('minute', now())
-sample by 1m`
+sample by ${mappedSampleBy[metricDuration]}`
 
 export const latency = (id: string, metricDuration: MetricDuration) => `
 select
@@ -31,5 +45,5 @@ and created > date_trunc('minute', dateadd('d', -${minutesToDays(
   durationInMinutes[metricDuration],
 )}, now()))
 and created < date_trunc('minute', now())
-sample by 1m
+sample by ${mappedSampleBy[metricDuration]}
 `
