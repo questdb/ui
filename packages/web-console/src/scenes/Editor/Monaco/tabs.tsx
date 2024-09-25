7d603d3c79872cd838947493e143c4a3e9fc3771
Maciej Bodek <maciej.bodek@gmail.com>
2024-09-25 14:40:14 +0200

    Fix: tab sorting in reorder

--- packages/web-console/src/scenes/Editor/Monaco/tabs.tsx
+++ packages/web-console/src/scenes/Editor/Monaco/tabs.tsx
@@ -100,9 +100,9 @@ export const Tabs = () => {
     if (!beforeTab) {
       return
     }
-    let newTabs = buffers.filter(
-      (tab) => tab.id !== parseInt(tabId) && !tab.archived,
-    )
+    let newTabs = buffers
+      .filter((tab) => tab.id !== parseInt(tabId) && !tab.archived)
+      .sort((a, b) => a.position - b.position)
     newTabs.splice(toIndex, 0, beforeTab)
     newTabs.forEach(async (tab, index) => {
       await updateBuffer(tab.id as number, { position: index })
