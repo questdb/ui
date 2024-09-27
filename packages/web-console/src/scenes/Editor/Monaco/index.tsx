97a9c00619dab77ba1c733cbf44b928c654fdb65
Jaromir Hamala <jaromir.hamala@gmail.com>
2024-09-27 15:44:31 +0200

    experiment: support for Notice responses

--- packages/web-console/src/scenes/Editor/Monaco/index.tsx
+++ packages/web-console/src/scenes/Editor/Monaco/index.tsx
@@ -322,6 +322,20 @@ const MonacoEditor = () => {
               eventBus.publish(EventType.MSG_QUERY_SCHEMA)
             }
 
+            if (result.type === QuestDB.Type.NOTICE) {
+              dispatch(
+                actions.query.addNotification({
+                  content: (
+                    <Text color="foreground" ellipsis title={result.query}>
+                      {result.notice}: {result.query}
+                    </Text>
+                  ),
+                  type: NotificationType.NOTICE,
+                }),
+              )
+              eventBus.publish(EventType.MSG_QUERY_SCHEMA)
+            }
+
             if (result.type === QuestDB.Type.DQL) {
               setLastExecutedQuery(request.query)
               dispatch(
