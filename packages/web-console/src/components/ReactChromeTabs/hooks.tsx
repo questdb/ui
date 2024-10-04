8d1e1ad052678f2395bf0a1dc535b16d199af123
Vlad Ilyushchenko <bluestreak@gmail.com>
2024-10-04 18:33:41 +0100

    fix tab add

--- packages/web-console/src/components/ReactChromeTabs/hooks.tsx
+++ packages/web-console/src/components/ReactChromeTabs/hooks.tsx
@@ -193,16 +193,16 @@ export function useChromeTabs(listeners: Listeners, limit?: number) {
   }, [])
 
   const updateTab = useCallback((tabId: string, tab: TabProperties) => {
-    if (tabId !== tab.id) {
-      // tabs component is liable to call this to "update" already initialized
-      // tab to use different ID. This messes things up quite badly, so it is
-      // aborted here.
-      return
-    }
     const ele = ref.current?.querySelector(
       `[data-tab-id="${tabId}"]`,
     ) as HTMLDivElement
     if (ele) {
+      if (tabId !== tab.id) {
+        // tabs component is liable to call this to "update" already initialized
+        // tab to use different ID. This messes things up quite badly, so it is
+        // aborted here.
+        return
+      }
       chromeTabsRef.current?.updateTab(ele, { ...tab })
     } else {
       chromeTabsRef.current?.addTab(tab)
