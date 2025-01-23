2939b0a2f81d5b8df3a3909b1034c7b609cf9f0b
glasstiger <94906625+glasstiger@users.noreply.github.com>
2025-01-23 17:29:19 +0000

    chore(ui): run web console tests with context path (#372)
    * chore(ui): run web console tests with context path
    
    * chore(ui): run web console tests with context path
    
    * chore(ui): run web console tests with context path
    
    * chore(ui): run web console tests with context path
    
    * chore(ui): run web console tests with context path
    
    * chore(ui): run web console tests with context path
    
    * update submodule
    
    * update submodule
    
    * update submodule
    
    * QDB_HTTP_CONTEXT_PATH -> QDB_HTTP_CONTEXT_WEB_CONSOLE

--- packages/browser-tests/cypress/integration/console/telemetry.spec.js
+++ packages/browser-tests/cypress/integration/console/telemetry.spec.js
@@ -1,13 +1,11 @@
 /// <reference types="cypress" />
 
-const baseUrl = "http://localhost:9999";
-
 const toggleTelemetry = (enabled) => {
   // expected dataset format of the first row:
   // [id, enabled, version, os, package]
   cy.interceptQuery("telemetry_config LIMIT -1", "telemetryConfig", (req) => {
     return req.continue((res) => {
-      // enable telemetry to kick start the process on the client side
+      // enable telemetry to kick-start the process on the client side
       res.body.dataset[0][1] = enabled;
       return res;
     });
