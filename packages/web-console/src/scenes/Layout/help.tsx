04734300b11a96011c7c671d3fd04a54b4c17c60
Maciej Bodek <1871646+insmac@users.noreply.github.com>
2025-01-28 20:48:08 +0100

    fix(web-console): Make the whole menu item area clickable (#381)

--- packages/web-console/src/scenes/Layout/help.tsx
+++ packages/web-console/src/scenes/Layout/help.tsx
@@ -1,5 +1,10 @@
-import React from "react"
-import { Chat3, Command, Question } from "@styled-icons/remix-line"
+import React, { useEffect } from "react"
+import {
+  Chat3,
+  Command,
+  ExternalLink,
+  Question,
+} from "@styled-icons/remix-line"
 import {
   Discourse,
   Github,
@@ -13,9 +18,14 @@ import {
   Link,
   PopperToggle,
 } from "../../components"
-import { DropdownMenu, FeedbackDialog, Box } from "@questdb/react-components"
+import {
+  DropdownMenu,
+  FeedbackDialog,
+  ForwardRef,
+  Box,
+} from "@questdb/react-components"
 import { BUTTON_ICON_SIZE } from "../../consts"
-import { IconWithTooltip } from "../../components"
+import { IconWithTooltip, useKeyPress } from "../../components"
 import { useState, useCallback, useContext } from "react"
 import { QuestContext } from "../../providers"
 import { useSelector } from "react-redux"
@@ -31,8 +41,9 @@ const DropdownMenuContent = styled(DropdownMenu.Content)`
   background: ${({ theme }) => theme.color.backgroundDarker};
 `
 
-const DropdownMenuItem = styled(DropdownMenu.Item)`
+const DropdownMenuItem = styled(DropdownMenu.Item)<{ withlink?: string }>`
   color: ${({ theme }) => theme.color.foreground};
+  ${({ withlink }) => withlink && "padding: 0;"}
 `
 
 const TooltipWrapper = styled(Box).attrs({ justifyContent: "center" })`
@@ -46,11 +57,17 @@ const ShortcutsWrapper = styled.div`
   margin-top: 4.5rem;
 `
 
+const StyledLink = styled(Link)`
+  padding: 0.5rem 1rem;
+  width: 100%;
+`
+
 const MenuLink: React.FunctionComponent<{
   href: string
   text: string
-}> = ({ href, text, ...rest }) => (
-  <Link
+  icon: React.ReactNode
+}> = ({ href, text, icon, ...rest }) => (
+  <StyledLink
     color="foreground"
     hoverColor="foreground"
     href={href}
@@ -58,8 +75,14 @@ const MenuLink: React.FunctionComponent<{
     target="_blank"
     {...rest}
   >
-    {text}
-  </Link>
+    <Box align="center" gap="1.5rem">
+      {icon}
+      <Box align="center" gap="0.75rem">
+        {text}
+        <ExternalLink size="14px" />
+      </Box>
+    </Box>
+  </StyledLink>
 )
 
 export const Help = () => {
@@ -71,6 +94,13 @@ export const Help = () => {
   }, [])
   const [open, setOpen] = useState(false)
   const [feedbackOpen, setFeedbackOpen] = useState(false)
+  const escPress = useKeyPress("Escape")
+
+  useEffect(() => {
+    if (escPress && shortcutsPopperActive) {
+      setShortcutsPopperActive(false)
+    }
+  }, [escPress])
 
   return (
     <React.Fragment>
@@ -123,55 +153,64 @@ export const Help = () => {
           <DropdownMenuContent>
             <DropdownMenuItem
               onSelect={(e: Event) => e.preventDefault()}
+              onClick={() => setFeedbackOpen(true)}
               data-hook="help-link-contact-us"
             >
               <Chat3 size="18px" />
-              <Text color="foreground" onClick={() => setFeedbackOpen(true)}>
-                Contact us
-              </Text>
+              <Text color="foreground">Contact us</Text>
             </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Slack size="18px" />
-              <MenuLink
-                data-hook="help-link-slack"
-                href="https://slack.questdb.io/"
-                text="Slack community"
-              />
+            <DropdownMenuItem asChild withlink="true">
+              <ForwardRef>
+                <MenuLink
+                  data-hook="help-link-slack"
+                  href="https://slack.questdb.io/"
+                  text="Slack community"
+                  icon={<Slack size="18px" />}
+                />
+              </ForwardRef>
             </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Discourse size="18px" />
-              <MenuLink
-                data-hook="help-link-community"
-                href="https://community.questdb.io/"
-                text="Public forum"
-              />
+            <DropdownMenuItem asChild withlink="true">
+              <ForwardRef>
+                <MenuLink
+                  data-hook="help-link-community"
+                  href="https://community.questdb.io/"
+                  text="Public forum"
+                  icon={<Discourse size="18px" />}
+                />
+              </ForwardRef>
             </DropdownMenuItem>
-            <DropdownMenuItem>
-              <StackOverflow size="18px" />
-              <MenuLink
-                data-hook="help-link-stackoverflow"
-                href="https://stackoverflow.com/tags/questdb"
-                text="Stack Overflow"
-              />
+            <DropdownMenuItem asChild withlink="true">
+              <ForwardRef>
+                <MenuLink
+                  data-hook="help-link-stackoverflow"
+                  href="https://stackoverflow.com/tags/questdb"
+                  text="Stack Overflow"
+                  icon={<StackOverflow size="18px" />}
+                />
+              </ForwardRef>
             </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Question size="18px" />
-              <MenuLink
-                data-hook="help-link-web-console-docs"
-                href="https://questdb.io/docs/develop/web-console/"
-                text="Web Console Docs"
-              />
+            <DropdownMenuItem asChild withlink="true">
+              <ForwardRef>
+                <MenuLink
+                  data-hook="help-link-web-console-docs"
+                  href="https://questdb.io/docs/develop/web-console/"
+                  text="Web Console Docs"
+                  icon={<Question size="18px" />}
+                />
+              </ForwardRef>
             </DropdownMenuItem>
             <DropdownMenuItem onClick={() => handleShortcutsToggle(true)}>
               <Command size="18px" />
               <Text color="foreground">Shortcuts</Text>
             </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Github size="18px" />
-              <MenuLink
-                href={`https://github.com/questdb/ui/commit/${process.env.COMMIT_HASH}`}
-                text={`Commit id: ${process.env.COMMIT_HASH}`}
-              />
+            <DropdownMenuItem asChild withlink="true">
+              <ForwardRef>
+                <MenuLink
+                  href={`https://github.com/questdb/ui/commit/${process.env.COMMIT_HASH}`}
+                  text={`Commit id: ${process.env.COMMIT_HASH}`}
+                  icon={<Github size="18px" />}
+                />
+              </ForwardRef>
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu.Portal>
