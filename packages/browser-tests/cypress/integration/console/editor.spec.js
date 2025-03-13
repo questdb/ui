c3889379594119593fd6e236551eeb0d84f01e4a
goodroot <9484709+goodroot@users.noreply.github.com>
2025-03-13 10:51:50 -0700

    folds https://github.com/questdb/ui/pull/388

--- packages/browser-tests/cypress/integration/console/editor.spec.js
+++ packages/browser-tests/cypress/integration/console/editor.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-const contextPath = process.env.QDB_HTTP_CONTEXT_WEB_CONSOLE || ""
+const contextPath = process.env.QDB_HTTP_CONTEXT_WEB_CONSOLE || "";
 const baseUrl = `http://localhost:9999${contextPath}`;
 
 const getTabDragHandleByTitle = (title) =>
@@ -504,14 +504,23 @@ describe("editor tabs history", () => {
     cy.get(getTabDragHandleByTitle("SQL 1")).drag(
       getTabDragHandleByTitle("SQL")
     );
+    // subsequent wait()x3: waiting period ensures models are updated
+    cy.wait(250);
     cy.getEditorTabs().first().should("contain", "SQL 1");
     cy.getEditorTabs().last().should("contain", "SQL");
-    cy.wait(1000);
+    cy.wait(250);
     cy.get(getTabDragHandleByTitle("SQL 1")).drag(
       getTabDragHandleByTitle("SQL")
     );
     cy.getEditorTabs().first().should("contain", "SQL");
     cy.getEditorTabs().last().should("contain", "SQL 1");
+    cy.wait(250);
+    // Test model disposal logic 
+    // Should dispose of prior models when switching active tabs
+    cy.window().then((window) => {
+      const editorModels = window.monaco.editor.getModels();
+      expect(editorModels.length).to.equal(1);
+    });
   });
 });
 
