555be6f030a892ffa5f55741ff2d863465b9d929
emrberk <emreberk5699@gmail.com>
2025-04-14 15:19:14 +0300

    fix(web-console): workaround for safari copy schema mechanism

--- packages/web-console/src/utils/copyToClipboard.ts
+++ packages/web-console/src/utils/copyToClipboard.ts
@@ -1,22 +1,29 @@
-export const copyToClipboard = async (textToCopy: string) => {
-  if (navigator.clipboard && window.isSecureContext) {
-      await navigator.clipboard.writeText(textToCopy);
-  } else {
-      const textArea = document.createElement("textarea");
-      textArea.value = textToCopy;
-          
-      textArea.style.position = "absolute";
-      textArea.style.left = "-999999px";
-          
-      document.body.prepend(textArea);
-      textArea.select();
-
-      try {
-          document.execCommand('copy');
-      } catch (error) {
-          console.error(error);
-      } finally {
+export const copyToClipboard = (textToCopy: string): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      if (navigator.clipboard && window.isSecureContext) {
+        setTimeout(() => {
+          navigator.clipboard.writeText(textToCopy)
+            .then(resolve)
+            .catch(reject);
+        });
+      } else {
+        const textArea = document.createElement("textarea");
+        textArea.value = textToCopy;
+  
+        textArea.style.position = "absolute";
+        textArea.style.left = "-999999px";
+        document.body.prepend(textArea);
+        textArea.select();
+  
+        try {
+          const success = document.execCommand('copy');
+          success ? resolve() : reject(new Error('Copy command failed'));
+        } catch (err) {
+          reject(err);
+        } finally {
           textArea.remove();
+        }
       }
-  }
-}
+    });
+  };
+  
