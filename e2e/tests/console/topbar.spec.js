c93d767b8b6e6d975f2411c7f4ddbd714105f5cc
emrberk <emreberk5699@gmail.com>
2026-09-23 09:47:41 +0300

    topbar spec ordering

--- e2e/tests/console/topbar.spec.js
+++ e2e/tests/console/topbar.spec.js
@@ -44,11 +44,29 @@ describe("TopBar", () => {
     )
   })
 
+  it("should not show the instance tooltip when the description is empty", () => {
+    // Given
+    cy.getByDataHook("topbar-instance-badge").realHover()
+    cy.getByDataHook("topbar-instance-edit-icon").should("be.visible")
+    cy.getByDataHook("topbar-instance-edit-icon").click()
+    cy.getByDataHook("topbar-instance-name-input").type("test-instance-1")
+    cy.getByDataHook("topbar-instance-type-select").click()
+    cy.getByDataHook("topbar-instance-type-option-testing").click()
+    cy.getByDataHook("topbar-instance-save-button").click()
+    cy.getByDataHook("topbar-instance-save-button").should("not.exist")
+
+    // When
+    cy.getByDataHook("topbar-instance-icon").realHover()
+
+    // Then
+    cy.getByDataHook("topbar-instance-tooltip").should("not.exist")
+  })
+
   it("should change the instance name, description, and type", () => {
     cy.getByDataHook("topbar-instance-badge").realHover()
     cy.getByDataHook("topbar-instance-edit-icon").should("be.visible")
     cy.getByDataHook("topbar-instance-edit-icon").click()
-    cy.getByDataHook("topbar-instance-name-input").type("test-instance")
+    cy.getByDataHook("topbar-instance-name-input").clear().type("test-instance")
     cy.getByDataHook("topbar-instance-type-select").click()
     cy.getByDataHook("topbar-instance-type-option-testing").click()
     cy.getByDataHook("topbar-instance-description-input").type(
@@ -63,21 +81,6 @@ describe("TopBar", () => {
     cy.getByDataHook("topbar-instance-tooltip").should("be.visible")
     cy.contains("test description of the test instance").should("be.visible")
     cy.contains("testing").should("be.visible")
-  })
-
-  it("should not show the instance tooltip when the description is empty", () => {
-    // Given
-    cy.getByDataHook("topbar-instance-badge").realHover()
-    cy.getByDataHook("topbar-instance-edit-icon").click()
-    cy.getByDataHook("topbar-instance-description-input").clear()
-    cy.getByDataHook("topbar-instance-save-button").click()
-    cy.getByDataHook("topbar-instance-save-button").should("not.exist")
-
-    // When
-    cy.getByDataHook("topbar-instance-icon").realHover()
-
-    // Then
-    cy.getByDataHook("topbar-instance-tooltip").should("not.exist")
   })
 })
 
